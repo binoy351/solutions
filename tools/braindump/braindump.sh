052e3ea1a64a8a9c97a1349dd1adcc3e00088a9e
Binoy_thomas <30573974+binoy351@users.noreply.github.com>
2023-12-21 08:58:08 +0530

    Update braindump.sh
    - Updated commands to include the namespace
    - `kubectl describe` doesnt support output format, changed command to `kubectl get`
    - Small correction in `deployment` keyword
    - `tar -x` conflicts with `-c`. Added `-v` to display the files being compressed

--- tools/braindump/braindump.sh
+++ tools/braindump/braindump.sh
@@ -3,18 +3,19 @@
 # This is your helm deployment name
 # If you need to find this, please use "helm list" to find the Cortex deployment name
 HELM_MANIFEST="cortex"
+NAMESPACE="cortex"
 
 # This is your backend deployment name.
 # If you need to find this, please use "kubectl get deployments"
 CORTEX_BACKEND_DEPLOYMENT="cortex-deployment-backend"
 
 mkdir braindump; cd braindump/
-helm get manifest $HELM_MANIFEST > helm-manifest.yaml
-kubectl describe deployments > deployments.json
-kubectl describe po -o json -l cortex > pods.json
-kubectl logs --since=8h deploymnent/$CORTEX_BACKEND_DEPLOYMENT > backend-logs.log
+helm get manifest $HELM_MANIFEST -n $NAMESPACE > helm-manifest.yaml
+kubectl describe deployments -n $NAMESPACE > deployments.json
+kubectl get po -o json -l cortex -n $NAMESPACE > pods.json
+kubectl logs --since=8h deployment/$CORTEX_BACKEND_DEPLOYMENT -n $NAMESPACE > backend-logs.log
 cd ..
-tar -xzcf braindump.tar.gz braindump
+tar -cvzf braindump.tar.gz braindump
 
 echo "\n\n############################## CORTEX BRAINDUMP COMPLED ###################################"
 echo "Please send the generated file \'braindump.tar.gz\' to Cortex support at help@cortex.io!"
